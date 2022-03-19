3e9cd13021d3fe26e340dc0d0587ca89eba45a0c
Denis Ilyin <DenisIlin2@yandex.ru>
2022-03-19 13:38:47 +0300

    lastday

--- js/script.js
+++ js/script.js
@@ -1,3 +1,45 @@
+let one=document.querySelector('.one');
+let two=document.querySelector('.two');
+let tree=document.querySelector('.three');
+let four=document.querySelector('.four');
+
+
+let StepOne=function(){
+  one.style.display='flex';
+  two.style.display='none';
+  tree.style.display='none';
+  four.style.display='none';
+}
+let StepTwo=function(){
+  one.style.display='none';
+  two.style.display='flex';
+  tree.style.display='none';
+  four.style.display='none';
+}
+let StepThree=function(){
+  one.style.display='none';
+  two.style.display='none';
+  tree.style.display='flex';
+  four.style.display='none';
+}
+let StepFour=function(){
+  one.style.display='none';
+  two.style.display='none';
+  tree.style.display='none';
+  four.style.display='flex';
+}
+
+let showinfo=function(e){
+  let num=0;
+  let elem= document.querySelector(`.${e}`);
+  elem.classList.toggle('displaynone');
+
+}
+
+
+
+
+
 
 const swiper = new Swiper('.swiper', {
     // Optional parameters
@@ -10,3 +52,8 @@ const swiper = new Swiper('.swiper', {
     },
   
   });
+
+
+
+
+  
